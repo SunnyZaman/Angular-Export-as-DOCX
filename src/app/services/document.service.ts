bb594fcd76644578149c858cdcf0a18764781b7f
Zaman <Sunny.Zaman@ontario.ca>
2020-04-27 17:05:12 -0400

    json data to docx

--- src/app/services/document.service.ts
+++ src/app/services/document.service.ts
@@ -1,23 +1,46 @@
 import { Injectable } from '@angular/core';
-import { Document, Paragraph, TextRun, AlignmentType, HeadingLevel } from "docx";
+import { Document, Paragraph, TextRun, AlignmentType, HeadingLevel, Table, TableRow, TableCell } from "docx";
 @Injectable({
     providedIn: "root"
 })
 export class DocumentService {
-    public create(data): Document {
+    public create(jsonData): Document {
         const document = new Document({
             description: "Showing the basketball players who scored the most in the NBA",
             title: "NBA Top Scorers"
         });
+        const title = new Paragraph({
+            text: "NBA Players: Leading Scorers",
+            alignment: AlignmentType.CENTER,
+            heading: HeadingLevel.TITLE
+        });
+        const keys = Object.keys(jsonData[0]); //grab an object keys
+        const rows = this.getTableRows(jsonData,keys);
+        const table = new Table({
+            rows: rows
+        });
         document.addSection({
-            children: [
-                new Paragraph({
-                    text: "NBA Players: Leading Scorers",
-                    alignment: AlignmentType.CENTER,
-                    heading: HeadingLevel.TITLE
-                }),
-            ],
+            children: [title, table]
         });
         return document;
+    }
+    getTableRows(jsonData,keys){
+        let cells = [];
+        let rows = [];
+        jsonData.forEach(data=>{            
+            keys.forEach(key=>{
+                cells.push(
+                    new TableCell({
+                        children: [new Paragraph(data[key]+"")]
+                    })
+                )
+            })            
+            rows.push(
+                new TableRow({
+                    children: cells
+                }))
+                cells = []; //reset cell array
+        })
+        return rows;
     }
 }
