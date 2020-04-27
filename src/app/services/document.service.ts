c9748d7fbb168bad5b618e3a24287f048f2af427
Zaman <Sunny.Zaman@ontario.ca>
2020-04-27 15:30:47 -0400

    Added a title to the word doc

--- src/app/services/document.service.ts
+++ src/app/services/document.service.ts
@@ -1,12 +1,23 @@
 import { Injectable } from '@angular/core';
-import { Document } from "docx";
+import { Document, Paragraph, TextRun, AlignmentType, HeadingLevel } from "docx";
 @Injectable({
     providedIn: "root"
-  })
+})
 export class DocumentService {
     public create(data): Document {
-        const document = new Document();
-
+        const document = new Document({
+            description: "Showing the basketball players who scored the most in the NBA",
+            title: "NBA Top Scorers"
+        });
+        document.addSection({
+            children: [
+                new Paragraph({
+                    text: "NBA Players: Leading Scorers",
+                    alignment: AlignmentType.CENTER,
+                    heading: HeadingLevel.TITLE
+                }),
+            ],
+        });
         return document;
     }
 }
